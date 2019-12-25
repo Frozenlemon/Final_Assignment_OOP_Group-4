7b859b84ac41d5ffce5e508119b15934502ca3a5
Frozenlemon <50400094+Frozenlemon@users.noreply.github.com>
2019-12-25 10:46:10 +0700

    update Menu class
    + added trasition code from Menu class to controller for 2 new events.
    + added 2 skeleton fucntion to highlight a particular button and turn off the highlight

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,10 +1,12 @@
 package view;
 
+import controller.MainController;
 import javafx.beans.NamedArg;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 
 public class Menu {
@@ -47,6 +49,36 @@ public class Menu {
     public void translate(@NamedArg("x pixels") double x, @NamedArg("y pixels") double y){
         menuArea.setTranslateX(x);
         menuArea.setTranslateY(y);
+    }
+
+    public void highlightButton_On(@NamedArg("Button type: 0: rollDice, 1: musicButton, 2: stopButton") int type){
+        /*
+        * function to turn on highlight for a button. Button selected by the type provided in argument
+        */
+        System.out.println("highlight on");
+    }
+
+    public void highlightButton_Off(@NamedArg("Button type: 0: rollDice, 1: musicButton, 2: stopButton") int type){
+        /*
+         * function to turn off highlight for a button. Button selected by the type provided in argument
+         */
+        System.out.println("highlight off");
+    }
+
+    @FXML
+    private void mouseHover_On(MouseEvent event){
+        if (event.getSource() instanceof Button){
+            Button source = (Button) event.getSource();
+            MainController.getInstance().highlight_On(source.getId());
+        }
+    }
+
+    @FXML
+    private void mouseHover_Off(MouseEvent event){
+        if (event.getSource() instanceof Button){
+            Button source = (Button) event.getSource();
+            MainController.getInstance().highlight_Off(source.getId());
+        }
     }
 
 }
