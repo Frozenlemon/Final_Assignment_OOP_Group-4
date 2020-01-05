19de54e47e1ea4ef3445132f44bdbe2e0828b766
s3672214 <59038265+s3672214@users.noreply.github.com>
2020-01-05 12:47:25 +0700

    new stuff

---
+++ src/view/StartMenu.java
@@ -0,0 +1,25 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.StackPane;
+
+import java.awt.*;
+
+public class StartMenu {
+
+    @FXML
+    private StackPane startMenuSceen;
+    @FXML
+    private Button startGameButton;
+    @FXML
+    private Button exitGameButton;
+    @FXML
+    private ToggleButton musicButton;
+
+    public StartMenu() {}
+
+    public Button getMenu() {return startGameButton; }
+
+    public
+}
