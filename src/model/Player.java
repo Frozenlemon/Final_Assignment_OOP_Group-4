099a36cb30f8feb75a2e96b98f85a76ecaddfa1c
s3672214 <59038265+s3672214@users.noreply.github.com>
2020-01-13 17:53:41 +0700

    Update Player.java v0.1

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,18 +1,23 @@
 package model;
 
+import controller.ModelController;
+import model.Horse;
 import javafx.scene.paint.Color;
 
 public class Player {
 
     private Horse[] horses;
     private int colorCode;
-
+    private Dice[] dices;
     public Player(int colorCode){
         this.colorCode = colorCode;
-        horses = new Horse[4];
+        horses = new Horse[]{new Horse(colorCode*4+0), new Horse(colorCode*4+1), new Horse(colorCode*4+2), new Horse(colorCode*4+3)};
+        dices = new Dice[]{new Dice()};
     }
 
     public boolean isHuman(){
+        if (this instanceof Player)
+            return true;
         return false;
     }
 
@@ -25,7 +30,30 @@ public class Player {
         return horses[id];
     }
 
-    public void autoMove(){
+    public void autoMove() {
+        ModelController.getInstance().rollDice();
+        int[] dice_values = ModelController.getInstance().getAllDiceValue();
+        //while (ModelController.getInstance().getAnimationCount() != 0) {
+        //}
+        for (int i = 0; i < dices.length; i++) {
+            int init_possible = -1;
+            int init_horse = -1;
+            for (int j = 0; j < horses.length; j++) {
+                if (init_possible < getHorse(j).checkMove(dice_values[i])){
+                    init_horse = j;
+                    init_possible = getHorse(j).checkMove(dice_values[i]);
+                }
+            }
+            getHorse(init_horse).move(init_possible, dice_values[i]);
+            //while (ModelController.getInstance().getAnimationCount() != 0);
+            }
+        ModelController.getInstance().nextPlayer();
+    }
 
+    private boolean checkDice(Dice[] dices){
+        for (Dice dice: dices)
+            while (!dice.isUsed())
+                return true;
+        return false;
     }
 }
