2913e34ea230bf3dfec1724adb506c566713137b
s3678437 <45480252+s3678437@users.noreply.github.com>
2020-01-15 17:48:43 +0700

    Edit

--- src/view/StartMenu.java
+++ src/view/StartMenu.java
@@ -1,3 +1,28 @@
+/*
+  RMIT University Vietnam
+  Course: INTE2512 Object-Oriented Programming
+  Semester: 2019C
+  Assessment: Final Project
+  Created date: 23/03/2019
+  By: Le Minh s3757324
+      Ho Duy Hoang s3672214
+      Pham Thanh Dat s3678437
+      Ta Quoc Thang s3713564
+      Le Nguyen Thien Phu s3639855
+  Last modified: 15/01/2020
+  By: Le Minh s3757324
+      Ho Duy Hoang s3672214
+      Pham Thanh Dat s3678437
+      Ta Quoc Thang s3713564
+      Le Nguyen Thien Phu s3639855
+  Acknowledgement:
+[1] "Java Internationalization - javatpoint", www.javatpoint.com, 2020. [Online]. Available: https://www.javatpoint.com/internationalization-in-java. [Accessed: 14- Jan- 2020]
+[2]"Internationalizing the Sample Program (The Java™ Tutorials > Internationalization > Introduction)", Docs.oracle.com, 2020. [Online]. Available: https://docs.oracle.com/javase/tutorial/i18n/intro/steps.html?fbclid=IwAR00AiifBkc8cmbF43D0YEUM6ZYayCq_ldGYnVxZBzE0xlL_-2n-5ElxSeE. [Accessed: 14- Jan- 2020]
+[3]Tutorials.jenkov.com, 2020. [Online]. Available: http://tutorials.jenkov.com/java-internationalization/resourcebundle.html?fbclid=IwAR2FqduxMXmdSdnltj4q7BIrOCbadtPCiSEWwg2da_mvY71n-rPZHuQEG58. [Accessed: 14- Jan- 2020]
+[4]"Code Conventions for the Java Programming Language: 9. Naming Conventions", Oracle.com, 2020. [Online]. Available: https://www.oracle.com/technetwork/java/codeconventions-135099.html?fbclid=IwAR2ZlGNIfZqOB3fUjnW0A5dKgyPZFeaq8XKSgGbbfc6qBrw3dnmC-skDNdo. [Accessed: 14- Jan- 2020]
+[5]"JavaFX Playing Audio - javatpoint", www.javatpoint.com, 2020. [Online]. Available: https://www.javatpoint.com/javafx-playing-audio?fbclid=IwAR3WFDkfH5LvjWqk6OCoY55aLShLcb4VWCM7IFAyA-JkZBAYRlSpz0U-hFc. [Accessed: 14- Jan- 2020]
+[6]"CSS Examples", W3schools.com, 2020. [Online]. Available: https://www.w3schools.com/css/css_examples.asp. [Accessed: 14- Jan- 2020]
+*/
 package view;
 
 import controller.ViewController;
@@ -14,55 +39,6 @@ public class StartMenu {
 
     public StartMenu(){}
 
-    /*public StartMenu() {
-        Language.setLanguageText(startMenuSceen.getChildrenUnmodifiable());
-    }*/
-
-//    public StackPane getStartMenuScene(){
-//        return startMenuScene;
-//    }
-//    public Button getStartGameButton() {
-//        return startGameButton; }
-//
-//    public Button getExitGameButton() {
-//        return exitGameButton;
-//    }
-
-//    public void highlightButton_On(@NamedArg("Button type: 0: startGameMenu, 1: exitGameMenu, 2: musicButton") int type) {
-//        /*
-//         * function to turn on highlight for a button. Button selected by the type provided in argument
-//         */
-//        if (type ==0 || type==1 || type ==2){
-//            DropShadow shadow = new DropShadow();
-////            exitGameButton.setEffect(new javafx.scene.effect.DropShadow());
-//
-//
-//        }}
-//
-//    public void highlightButton_Off(@NamedArg("Button type: 0: rollDice, 1: musicButton, 2: stopButton") int type) {
-//        /*
-//         * function to turn off highlight for a button. Button selected by the type provided in argument
-//         */
-//        if (type ==0 || type==1 || type ==2){
-//            getExitGameButton().setEffect(null);
-//        }
-//    }
-//
-//    @FXML
-//    private void mouseHover_On(MouseEvent event){
-//        if (event.getSource() instanceof javafx.scene.control.Button){
-//            javafx.scene.control.Button source = (javafx.scene.control.Button) event.getSource();
-////            ModelController.getInstance().highlight_On(source.getId());
-//        }
-//    }
-//
-//    @FXML
-//    private void mouseHover_Off(MouseEvent event){
-//        if (event.getSource() instanceof javafx.scene.control.Button){
-//            javafx.scene.control.Button source = (javafx.scene.control.Button) event.getSource();
-////            ModelController.getInstance().highlight_Off(source.getId());
-//        }
-//    }
 
     public StackPane getStartMenu(){
         return this.startMenuScene;
